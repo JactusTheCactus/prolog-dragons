ae9ae13c7da9981ffaccf79a5b2aeee04f86de60
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-03-30 05:03:16 -0700

    -

---
+++ dragon.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -uo pipefail
+get() {
+	local -n _out=$1
+	msg=$2
+	shift 2
+	args=("$@")
+	while true; do
+		echo "$msg"
+		for i in "${!args[@]}"; do
+			echo "$((i + 1)). ${args[$i]}"
+		done
+		# shellcheck disable=SC2162
+		read -n 1 -s sel
+		if (( sel >= 1 && sel <= 3)); then
+			_out=${args[$((sel - 1))]}
+			break
+		else
+			clear
+			echo "'$sel' is invalid. Try again." >&2
+		fi
+	done
+}
+get env 'Choose an environment:' Land Sea Sky
+# shellcheck disable=2154
+echo "<$env>"
